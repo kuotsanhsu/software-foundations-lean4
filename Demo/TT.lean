c7ff5450e53f5e8117e8017a36c6ab3f89b50440
learningstud <learningstud@gmail.com>
2023-08-09 18:02:32 +0800

    Notation `::` for `Con.ext`

--- Demo/TT.lean
+++ Demo/TT.lean
@@ -12,11 +12,13 @@ inductive Con
   | emp
   | ext (Γ: Con) (σ: Ty)
 
+@[inherit_doc] infixr:67 " :: " => Con.ext
+
 inductive Var: Con → Ty → Type
-  | zero {Γ σ}: Var (.ext Γ σ) σ
-  | succ {Γ σ τ}: Var Γ σ → Var (.ext Γ τ) σ
+  | zero {Γ σ}: Var (Γ::σ) σ
+  | succ {Γ σ τ}: Var Γ σ → Var (Γ::τ) σ
 
 inductive Tm: Con → Ty → Type
   | var {Γ σ}: Var Γ σ → Tm Γ σ
   | app {Γ σ τ}: Tm Γ (.map σ τ) → Tm Γ σ → Tm Γ τ
-  | all {Γ σ τ}: Tm (.ext Γ σ) τ → Tm Γ (.map σ τ)
+  | all {Γ σ τ}: Tm (Γ::σ) τ → Tm Γ (.map σ τ)
